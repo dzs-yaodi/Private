f889ce3f7297aef3f9a3948a0baab51f47e9ae42
Admin <837156540@qq.com>
2022-02-15 21:34:08 +0800

    增加popupwindow

--- app/src/main/java/com/xw/aprivate/MainActivity.java
+++ app/src/main/java/com/xw/aprivate/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.xw.privatelib.ManagerDialog;
 import com.xw.privatelib.ui.ReportActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = findViewById(R.id.btn);
+        ManagerDialog managerDialog = new ManagerDialog(this);
 
-        findViewById(R.id.btn).setOnClickListener(v -> {
-            Intent intent = new Intent(this, ReportActivity.class);
-            startActivity(intent);
+        btn.setOnClickListener(v -> {
+//            Intent intent = new Intent(this, ReportActivity.class);
+//            startActivity(intent);
+            managerDialog.showPopupwindow(btn);
         });
     }
 }
